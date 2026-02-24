b1cd15745007a747e59d70d7cff8a21257476a78
mohammed <mohammed@DESKTOP-KKM3BU9>
2026-02-24 11:53:34 +0200

    DocumentEdit

---
+++ MyErp.Api/Controllers/DocumentController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Logger;
+using Microsoft.AspNetCore.Mvc;
+using MyErp.Core.DTO;
+using MyErp.Core.Global;
+using MyErp.Core.HTTP;
+using MyErp.Core.Interfaces;
+using MyErp.Core.Models;
+using MyErp.Core.Services;
+using MyErp.Core.Validation;
+using MyErp.EF.DataAccess;
+using MyErp.EF.Repositories;
+
+namespace MyErp.Api.Controllers
+{
+    [Route("[controller]/")]
+    [ApiController]
+    public class DocumentController : Controller
+    {
+        DocumentServices DocumentServices;
+        private readonly IMapper _mapper;
+
+        public DocumentController(ApplicationDbContext dBContext, IMapper mapper)
+        {
+            UnitOfWork unitOfWork = new UnitOfWork(dBContext);
+            _mapper = mapper;
+            DocumentServices = new DocumentServices(unitOfWork, _mapper);
+        }
+
+        // GET ALL
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetDocumentList()
+        {
+            var result = await DocumentServices.getDocumentList();
+            var resultWithStatusCode =
+                ResponseStatusCode<Document>.GetApiResponseCode(result, "HttpGet");
+
+            return resultWithStatusCode;
+        }
+
+        // GET BY ID
+        [HttpGet("getById")]
+        public async Task<IActionResult> GetDocument(int id)
+        {
+            var result = await DocumentServices.getDocument(id);
+            var resultWithStatusCode =
+                ResponseStatusCode<Document>.GetApiResponseCode(result, "HttpGet");
+
+            return resultWithStatusCode;
+        }
+
+        // UPDATE
+        [HttpPut("updateById")]
+        public async Task<IActionResult> PutDocument(
+            int id,
+            [FromForm] DocumentDTO documentUpdated)
+        {
+            var result = await DocumentServices.updateDocument(id, documentUpdated);
+
+            var resultWithStatusCode =
+                ResponseStatusCode<Document>.GetApiResponseCode(result, "HttpPut");
+
+            return resultWithStatusCode;
+        }
+
+        // ADD
+        [HttpPost("add")]
+        public async Task<IActionResult> AddDocument(
+            [FromForm] DocumentDTO documents)
+        {
+            var result = await DocumentServices.addDocument(documents);
+
+            var resultWithStatusCode =
+                ResponseStatusCode<Document>.GetApiResponseCode(result, "HttpPost");
+
+            return resultWithStatusCode;
+        }
+
+        // DELETE
+        [HttpDelete("deleteById")]
+        public async Task<IActionResult> DeleteDocument(int id)
+        {
+            var result = await DocumentServices.deleteDocument(id);
+
+            var resultWithStatusCode =
+                ResponseStatusCode<Document>.GetApiResponseCode(result, "HttpDelete");
+
+            return resultWithStatusCode;
+        }
+    }
+}
---
+++ MyErp.Core/DTO/DoucmentDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyErp.Core.DTO
+{
+    public   class DocumentDTO
+    {
+        public string Name { get; set; }
+        public IFormFile Attachment { get; set; }
+    }
+}
